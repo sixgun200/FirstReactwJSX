48856b1addb96e48490a79786140731848b21c99
sixgun200 <sixgun200@outlook.com>
2020-03-03 18:10:53 -0700

    First Commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class MyHelloWorldComponent extends Component {
+    render() {
+        return <div>Hello World!</div>;
+    }
+}
+export default MyHelloWorldComponent;
